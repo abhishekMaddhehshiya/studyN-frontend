4b1bb15694d9d20438e291450a7c8d3cefb4f93f
abhishekMaddhehshiya <abhishekmaddheshiya829@gmail.com>
2026-02-03 13:02:40 +0530

    issue solved

--- src/components/dashboard/EnrolledCourses.jsx
+++ src/components/dashboard/EnrolledCourses.jsx
@@ -4,7 +4,7 @@ import { BiDotsVerticalRounded } from "react-icons/bi"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
-import { getUserEnrolledCourses } from "../../services/oprations/profileApi"
+import { getUserEnrolledCourses } from "../../services/oprations/profileAPI"
 
 export default function EnrolledCourses() {
   const { token } = useSelector((state) => state.auth)
